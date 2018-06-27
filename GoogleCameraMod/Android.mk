55d97fb996a8065777e3086578316b2611793729
aditya <adityas745@gmail.com>
2018-06-27 09:04:03 +0000

    Add Google Camera Port: UriKill

--- GoogleCameraMod/Android.mk
+++ GoogleCameraMod/Android.mk
@@ -1,10 +1,10 @@
 include $(CLEAR_VARS)
 LOCAL_MODULE := GoogleCameraMod
 LOCAL_MODULE_TAGS := optional
-LOCAL_SRC_FILES := GoogleCameraMod/GoogleCamera-Pixel2Mod-Arnova8G2-V7beta-test-9_jhenrique09_mod.apk
+LOCAL_SRC_FILES := GoogleCameraMod/MGC_5.1.018_Urikill_vXXII.apk
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_DEX_PREOPT := false
-LOCAL_OVERRIDES_PACKAGES := Camera2
+LOCAL_OVERRIDES_PACKAGES := Camera2 Snap Camera GoogleCamera
 include $(BUILD_PREBUILT)
